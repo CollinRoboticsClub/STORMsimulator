eda7effdd610dc1fa480ec7e902ebaab9fa2b78a
DriftWare07 <99690970+DriftWare07@users.noreply.github.com>
2024-09-23 13:52:22 -0500

    Robot movement added
    Press R to change between strafe and tank modes

---
+++ scenes/robot.gd
@@ -0,0 +1,44 @@
+extends RigidBody3D
+
+
+
+var mode = true
+@export var move_force = 50
+@export var rotation_force = 0.5
+
+
+
+func _physics_process(delta):
+	if Input.is_action_just_pressed("toggle"): mode = !mode
+	
+	if mode:
+		strafe_movement()
+	else:
+		tank_movement()
+		
+	
+
+
+func strafe_movement():
+	
+	
+	if Input.is_action_pressed("forward"):
+		apply_central_force(-transform.basis.z*move_force)
+	if Input.is_action_pressed("back"):
+		apply_central_force(transform.basis.z*move_force)
+	
+	if Input.is_action_pressed("left"):
+		apply_central_force(-transform.basis.x*move_force)
+	if Input.is_action_pressed("right"):
+		apply_central_force(transform.basis.x*move_force)
+
+func tank_movement():
+	if Input.is_action_pressed("forward"):
+		apply_central_force(-transform.basis.z*move_force)
+	if Input.is_action_pressed("back"):
+		apply_central_force(transform.basis.z*move_force)
+	
+	if Input.is_action_pressed("left"):
+		angular_velocity.y += rotation_force
+	if Input.is_action_pressed("right"):
+		angular_velocity.y -= rotation_force
