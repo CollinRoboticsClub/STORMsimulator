0dab2f3ce6ee09c5b9a6d10f1ef7744017eb0624
DriftWare07 <99690970+DriftWare07@users.noreply.github.com>
2024-10-31 14:39:36 -0500

    balls pick a random size when they instance
    Balls will now pick between 2" 3" and 4" when spawning.

--- scenes/bawl.gd
+++ scenes/bawl.gd
@@ -1,9 +1,16 @@
 extends RigidBody3D
 
+var sizes = [load("res://bawl sizes/smallball.tres"), load("res://bawl sizes/medball.tres"), load("res://bawl sizes/largeball.tres")]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	randomize()
+	print("siza changing")
+	var rng = randi_range(0,2)
+	$CollisionShape3D.shape = sizes[rng]
+	
+	$MeshInstance3D.mesh.radius = sizes[rng].radius
+	$MeshInstance3D.mesh.height = sizes[rng].radius*2
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
